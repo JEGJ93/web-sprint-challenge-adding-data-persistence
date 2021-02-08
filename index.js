becd02f669c1cef5480f1c65d9c0ec8cd534f7c2
Julio Gomez <jgod93@Julios-MacBook-Pro-20.local>
2021-02-07 19:41:47 -0500

    MvP

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // start your server here
 const server = require('./api/server.js')
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 server.listen(port, () => {
     console.log(`Running at http://localhost:${port}`)
